6b1adea3d79a22028bdd3d27d1920242d02a8948
Rick Sullivan <rsullivan@focusvision.com>
2017-04-06 14:38:04 -0700

    Try with model

--- app/controllers/flash.js
+++ app/controllers/flash.js
@@ -2,10 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   flashMessages: Ember.inject.service(),
+  store: Ember.inject.service(),
 
   actions: {
     addFlash() {
-      this.get('flashMessages').success('Flash!')
+      this
+        .get('store')
+        .createRecord('thing')
+        .save()
+        .then(() => {
+          this.get('flashMessages').success('Flash!')
+        })
     }
   }
 });
